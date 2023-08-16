809a04f47613a8ff94d47c581442c59498642d1c
MING0403 <mi.in.ah0403@gmail.com>
2023-08-16 15:08:00 +0900

    조건에 부합하는 중고거래 댓글 조회하기.sql

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -3,5 +3,6 @@ SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents,
        DATE_FORMAT(r.created_date, '%Y-%m-%d') AS CREATED_DATE
 FROM used_goods_board AS b JOIN used_goods_reply AS r
 ON b.board_id = r.board_id
+-- '2022-10월'인 리스트만 출력
 WHERE DATE_FORMAT(b.created_date, '%Y-%m') = '2022-10' OR DATE_FORMAT(r.created_date, '%Y-%m') = '2022-10'
 ORDER BY r.created_date ASC, b.title ASC;
