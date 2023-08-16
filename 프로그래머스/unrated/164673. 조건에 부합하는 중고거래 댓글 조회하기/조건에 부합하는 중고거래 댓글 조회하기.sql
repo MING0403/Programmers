bd363bc91cdd1ab2a9b378ac63e393d63ffd6bde
MING0403 <mi.in.ah0403@gmail.com>
2023-08-16 15:06:23 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents,
+       DATE_FORMAT(r.created_date, '%Y-%m-%d') AS CREATED_DATE
+FROM used_goods_board AS b JOIN used_goods_reply AS r
+ON b.board_id = r.board_id
+WHERE DATE_FORMAT(b.created_date, '%Y-%m') = '2022-10' OR DATE_FORMAT(r.created_date, '%Y-%m') = '2022-10'
+ORDER BY r.created_date ASC, b.title ASC;
